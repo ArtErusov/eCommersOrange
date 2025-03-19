4a8be6f45e478a08f19b942b58efa8e7b56dd8a7
Artem Erusov <erusovav@gmail.com>
2025-03-19 09:48:25 +0300

    made a placeholder for the header

---
+++ src/components/layout/Header/Header.jsx
@@ -0,0 +1,21 @@
+import styles from './styles.module.css'
+function Header() {
+    const cities = ["Москва", "Тула", "Краснодар", "Санкт-Петербург", "Екатеринбург", "Новосибирск", "Казань", "Нижний Новгород", "Ростов-на-Дону", "Владивосток"];
+
+    return (
+        <header>
+            <div className={styles.top_header}>
+                <div className={`${styles.container} ${styles.flex}`}>
+                    <div>Город: {cities[1]}</div>
+                    <p>Портфолио</p>
+                </div>
+            </div>
+            <div className={`${styles.container} ${styles.flex}`}>
+                    <div className={styles.temporary_filling}>Тут будет лого</div>
+                    <div className={styles.temporary_filling}>Тут будет поиск</div>
+                    <div className={styles.temporary_filling}>Тут будет корзина</div>
+            </div>            
+        </header>
+    )
+  }
+export default Header
