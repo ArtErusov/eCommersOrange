7af31706b96e01cbdc3068e30319edb13fd93c2b
Artem Erusov <erusovav@gmail.com>
2025-03-24 22:09:47 +0300

    Made  component AdvertisingBanner

--- src/pages/MainPage/components/BannersBlock/AdvertisingBanner.jsx
+++ src/pages/MainPage/components/BannersBlock/AdvertisingBanner.jsx
@@ -1,10 +1,44 @@
 import styles from './styles.module.css';
-import bannerIMG from '../../../../assets/images/bannerIMG.png';
+import banner1 from '../../../../assets/images/BannerImgOne.png';
+import banner2 from '../../../../assets/images/BannerImgTwo.png';
+import banner3 from '../../../../assets/images/BannerImgThree.png';
+import banner4 from '../../../../assets/images/BannerImgFour.png';
+
+import { useState, useEffect } from 'react';
 
 function AdvertisingBanner() {
+  const banners = [banner1, banner2, banner3, banner4];
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [banners.length]);
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? banners.length - 1 : prevIndex - 1));
+  };
+
   return (
     <div className={styles.banner}>
-      <img src={bannerIMG} alt="" />
+      <button className={styles.banner_prev} onClick={prevSlide}></button>
+      <img src={banners[currentIndex]} alt="Banner" />
+      <button className={styles.banner_next} onClick={nextSlide}></button>
+      <div className={styles.banner_dots}>
+        {banners.map((_, index) => (
+          <div
+            key={index}
+            className={currentIndex === index ? styles.banner_dot_active : styles.banner_dot}
+            onClick={() => setCurrentIndex(index)}
+          ></div>
+        ))}
+      </div>
     </div>
   );
 }
