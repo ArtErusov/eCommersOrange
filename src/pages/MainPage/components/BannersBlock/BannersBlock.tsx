0db86f59a5112afab130b18e910fe5c37daed732
Artem Erusov <erusovav@gmail.com>
2025-05-11 13:18:43 +0300

    Reworked the BannersBlock component using tailwindcss

--- src/pages/MainPage/components/BannersBlock/BannersBlock.tsx
+++ src/pages/MainPage/components/BannersBlock/BannersBlock.tsx
@@ -1,9 +1,7 @@
-import styles from './styles.module.css';
-
 import AdvertisingBanner from './AdvertisingBanner.tsx';
-import BannerCard from '../../../../components/ui/BannerCard/BannerCard.tsx';
+import BannerCard from '@/components/ui/BannerCard/BannerCard.tsx';
 import { FC } from 'react';
-import { Product } from '../../../../types/product.ts';
+import { Product } from '@/types/product.ts';
 
 interface ProductCardProps {
   item: Product;
@@ -11,7 +9,7 @@ interface ProductCardProps {
 
 const BannersBlock: FC<ProductCardProps> = ({ item }) => {
   return (
-    <div className={styles.banner_block}>
+    <div className="flex justify-between mt-[30px]">
       <AdvertisingBanner />
       <BannerCard item={item} />
     </div>
