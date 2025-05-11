f8070bdb8eeb02b677c38c523b8380d8c1cf96b6
Artem Erusov <erusovav@gmail.com>
2025-05-11 15:56:08 +0300

    Made some minor changes

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,10 +1,9 @@
 import { FC, useEffect, useState } from 'react';
-import { Product } from '../../types/product.ts';
+import { Product } from '@/types/product.ts';
 
 import BannersBlock from './components/BannersBlock/BannersBlock.tsx';
-import styles from './styles.module.css';
 import ProductBlok from './components/ProductBlok/ProductBlok.tsx';
-import Header from '../../components/layout/Header/Header.tsx';
+import Header from '@/components/layout/Header/Header.tsx';
 import BannerSkeleton from '@/components/ui/BannerSkeleton.tsx';
 
 const MainPage: FC = () => {
@@ -27,12 +26,11 @@ const MainPage: FC = () => {
   return (
     <>
       <Header />
-      <div className={styles.container}>
+      <div className="container">
         {isLoading ? <BannersBlock item={items[5]} /> : <BannerSkeleton />}
 
         <ProductBlok />
-
-        <div style={{ width: '100%', height: '120px', backgroundColor: 'lightgray' }}></div>
+        <div className="w-[full] h-[120px] bg-[var(--gray)]"></div>
         <p className="mt-[400px] text-[var(--orange)]">Ghbdtn vbh</p>
       </div>
     </>
